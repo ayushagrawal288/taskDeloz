5a668b710eef6c59eb3b166ca682ae5ae323c38c
Ayush Agrawal <ayushagrawal288@gmail.com>
2017-11-13 18:40:34 +0530

    edited task1

--- task1Deloz.py
+++ task1Deloz.py
@@ -14,8 +14,8 @@ data = json.loads(request.content)
 if request.ok:
 	# print(data.keys(),data['total_count'])
 	d = data['items'][:20]
-	print(len(d))
+	# print(len(d))
 	for i in range(len(d)):
 		# print(d[i]['name'],d[i]['score'],d[i]['full_name'])
 		file.write(d[i]['name']+'\t'+str(d[i]['score'])+'\t'+d[i]['full_name']+'\n')
-print(request.url,request.json)
+# print(request.url,request.json)
